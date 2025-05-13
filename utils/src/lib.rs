a12362bcbc9d34c039f037e2907e886153c1cf3f
Ez-FlawLess <amirali23mohammadi2@gmail.com>
2025-05-13 18:25:58 +0330

    [#78] Move GPIO base abstractions to the utils crate (#9)
    https://project.nexushome.ir/work_packages/78

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -1,4 +1,7 @@
 #![cfg_attr(not(test), no_std)]
 
+pub use paste::paste as utils_paste;
+
 pub mod gpio;
 pub mod vector_table;
+pub mod register;
---
+++ utils/src/register.rs
@@ -0,0 +1,68 @@
+use core::ptr::{read_volatile, write_volatile};
+
+pub struct Register<const ADDR: usize, const INDEX: usize, const SIZE: usize>;
+
+impl<const ADDR: usize, const INDEX: usize, const SIZE: usize> Register<ADDR, INDEX, SIZE> {
+    pub fn write(value: usize) {
+        let mask: usize = const { 
+            if SIZE == 0 {
+                0
+            } else if SIZE >= usize::BITS as usize {
+                usize::MAX
+            } else {
+                (1 << SIZE) - 1
+            }
+        };
+
+        let mask = mask << INDEX;
+        let value = value << INDEX;
+
+        // let value = value & mask;
+        let addr = ADDR as *mut usize;
+        
+        let current_value = unsafe {
+            read_volatile(addr)
+        };
+
+        let new_value = (current_value & !mask) | value;
+        unsafe {
+            write_volatile(addr, new_value);
+        }
+    }
+}
+
+pub fn write_register(addr: *mut usize, index: usize, size: usize, value: usize) {
+    let mask = if size == 0 {
+        0
+    } else if size >= usize::BITS as usize {
+        usize::MAX
+    } else {
+        (1 << size) - 1
+    };
+
+    let mask = mask << index;
+    let value = value << index;
+
+    let current_value = unsafe {
+        read_volatile(addr)
+    };
+
+    let new_value = (current_value & !mask) | value;
+    unsafe {
+        write_volatile(addr, new_value);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_write_register() {
+        let num = 0_usize;
+
+        write_register(&num as *const usize as *mut usize, 3, 2, 0b11);
+
+        assert_eq!(0b11000, num);
+    }
+}
